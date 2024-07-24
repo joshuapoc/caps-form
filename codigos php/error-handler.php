ce90d417cc3032776b62b00e066317614ae38e3c
polterncat <polterncat@gmail.com>
2024-07-24 10:03:07 +0200

    (2.0) Redireccion a index.php?error en caso de haber datos duplicados o de conexion fallida a la bd. Se agrego tambien el icono de CAPS al head

--- codigos php/error-handler.php
+++ codigos php/error-handler.php
@@ -3,7 +3,7 @@
 errores de envio. El url nuevo llevara un mensaje de 'enviado' o 'error' en caso
 de que hayan exepciones u errores*/
 if(isset($_GET['error'])){
-    echo "<script>alert('Datos duplicados o error de envio');</script>";
+    echo "<script>alert('Datos duplicados o error de envio/conexion');</script>";
 }elseif(isset($_GET['enviado'])){
     echo "<script>alert('Envio exitoso');</script>";
 }

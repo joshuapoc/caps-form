1b45e933e0515dbd1aea979bc6de327d6d5ac597
polterncat <polterncat@gmail.com>
2024-07-24 23:11:30 +0200

    (d.c) el codigo js lanza un prompt indicando si hay errores o los datos se registran exitosamente

--- codigos php/error-handler.php
+++ codigos php/error-handler.php
@@ -3,8 +3,35 @@
 errores de envio. El url nuevo llevara un mensaje de 'enviado' o 'error' en caso
 de que hayan exepciones u errores*/
 if(isset($_GET['error'])){
-    echo "<script>alert('Datos duplicados o error de envio/conexion');</script>";
+    echo "
+    <script>
+       popup_container.style.visibility='visible';
+       popup_message.innerHTML='Error de envio o datos ya existentes';
+       popup_message.style.color='red';
+       popup_logo.style.animation='girar 4s';
+       popup_logo.style.animationDelay='1s';
+       window.addEventListener('click',(e)=>{
+            if(e.target===popup_x || e.target===popup_container){
+                popup_container.style.visibility='hidden';
+                popup_logo.style.animation='';
+            }
+       });
+    </script>
+    ";
 }elseif(isset($_GET['enviado'])){
-    echo "<script>alert('Envio exitoso');</script>";
+    echo "
+    <script>
+       popup_container.style.visibility='visible';
+       popup_message.innerHTML='Envio exitoso';
+       popup_logo.style.animation='girar 4s';
+       popup_logo.style.animationDelay='1s';
+       window.addEventListener('click',(e)=>{
+            if(e.target===popup_x || e.target===popup_container){
+                popup_container.style.visibility='hidden';
+                popup_logo.style.animation='';
+            }
+       });
+    </script>
+    ";
 }
 ?>
